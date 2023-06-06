c31823f4664e3ab4852fecf73a7ced3bacb6347f
NEWUSER <duy9117@gmai.com>
2023-06-06 21:38:28 +0700

    Initialize project

---
+++ src/pages/public/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <FavoriteIcon />
+            <ThumbUpIcon />
+        </div>
+    );
+};
+
+export default Home;
